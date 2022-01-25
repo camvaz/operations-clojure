87a5d5e66d39ce746d4debd399f4b105da99e589
Victor Campos <Victor.Campos.-ND@disney.com>
2022-01-24 19:57:34 -0600

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to operations-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/operations_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns operations-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [operations-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
